e24d26f573f94878dba6cf2836ce6e7260b92b7e
kmova <kiran.mova@mayadata.io>
2021-01-27 16:49:51 +0000

    set builds on amd64 only for cloud build
    Signed-off-by: kmova <kiran.mova@mayadata.io>

--- .cloudbuild.sh
+++ .cloudbuild.sh
@@ -1,1 +1,9 @@
-./release-tools/cloudbuild.sh
+#! /bin/bash
+
+# At the moment, only amd64 builds are supported by the ./Dockerfile. 
+: ${CSI_PROW_BUILD_PLATFORMS:="linux amd64"}
+
+# shellcheck disable=SC1091
+. release-tools/prow.sh
+
+gcr_cloud_build
